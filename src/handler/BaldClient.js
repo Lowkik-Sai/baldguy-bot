700b0dd4f0b0ee2a360c104a71af363432f2aa2c
Allvaa <alvamailindo@gmail.com>
2020-03-06 17:25:22 +0700

    command handler

--- src/handler/BaldClient.js
+++ src/handler/BaldClient.js
@@ -1,5 +1,6 @@
 const { Client } = require("discord.js");
 const EventHandler = require("./EventHandler");
+const CommandHandler = require("./CommandHandler");
 const { resolve } = require("path");
 
 /**
@@ -13,10 +14,12 @@ class BaldClient extends Client {
     constructor(opt) {
         super(opt);
         this.eventHandler = new EventHandler(this, resolve(__dirname, "..", "events"));
+        this.commandHandler = new CommandHandler(this, resolve(__dirname, "..", "commands"));
     }
 
     build() {
         this.eventHandler.build();
+        this.commandHandler.build();
         this.login(process.env.TOKEN);
     }
 }
--- src/handler/CommandHandler.js
+++ src/handler/CommandHandler.js
@@ -1,4 +1,6 @@
+const { Collection } = require("discord.js");
 const { readdir } = require("fs");
+const { resolve } = require("path");
 
 /**
  * @class CommandHandler
@@ -17,11 +19,58 @@ class CommandHandler {
          * @type {String}
          */
         this.path = path;
+        /**
+         * @type {import("discord.js").Collection<String, Object>}
+         */
+        this.modules = new Collection();
+        /**
+         * @type {import("discord.js").Collection<String, Object>}
+         */
+        this.commands = new Collection();
     }
 
     build() {
-        readdir(this.path, (err, files) => {
+        readdir(this.path, (err, dirs) => {
             if (err) throw Error(err);
+            console.log(`Loading commands from ${dirs.length} categories.`);
+            for (const dir of dirs) {
+                const moduleDir = resolve(this.path, dir);
+                const moduleConf = {
+                    name: dir,
+                    path: moduleDir,
+                    commands: []
+                };
+                readdir(moduleDir, (err, files) => {
+                    if (err) throw Error(err);
+                    for (const file of files) {
+                        const commandPath = resolve(moduleDir, file);
+                        const command = new (require(commandPath));
+                        command.path = commandPath;
+                        command.module = moduleConf;
+                        moduleConf.commands.push(command);
+                        this.commands.set(command.name, command);
+                    }
+                });
+                this.modules.set(dir, moduleConf);
+            }
+            console.log("Loaded!");
+        });
+        this.client.on("message", message => {
+            const prefix = process.env.PREFIX;
+
+            if (message.author.bot) return;
+            if (!message.content.startsWith(prefix)) return;
+            const args = message.content.slice(prefix.length).trim().split(/ +/g);
+            const commandName = args.shift().toLocaleLowerCase();
+
+            const command = this.commands.get(commandName) || this.commands.find(c => c.aliases.includes(commandName));
+            try {
+                command.exec(this.client, message, args);
+            } catch (e) {
+                console.error(e.stack);
+            }
         });
     }
 }
+
+module.exports = CommandHandler;
