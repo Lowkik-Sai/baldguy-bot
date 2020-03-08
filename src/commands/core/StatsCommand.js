e65b5076ed1650bac281fb6d84e93daa0d61f96d
KurokuTetsuya <perumnas.good@gmail.com>
2020-03-08 13:30:57 +0700

    Fix bug

--- src/commands/core/StatsCommand.js
+++ src/commands/core/StatsCommand.js
@@ -21,45 +21,53 @@ class StatsCommand extends Command {
      * @param {Array<String>} args
      */
     exec(client, message, args) {
-        let owner, uptime;
-        require("child_process").exec("uptime -p", (_, out) => { uptime = out });
-        client.config.owners.forEach(own => { owner = []; const fetch = client.users.resolve(own); owner.push(fetch) });
-        const memory = client.util.bytesToSize(process.memoryUsage().heapUsed);
-        const total = client.util.bytesToSize(require("os").totalmem);
-        const embed = new MessageEmbed()
-        .setAuthor(`${client.user.tag} Statistic`, client.util.getAvatar(client.user))
-        .setColor("#7289DA")
-        .setThumbnail(client.util.getGuildIcon(client.guilds.cache.get("332877090003091456")))
-        .addField("📊 Information", `
-\`\`\`css
+        let owner = [];
+        let uptime = "";
+        require("child_process").exec("uptime -p", (_, out) => {
+            client.config.owners.forEach(own => { const fetch = client.users.resolve(own); owner.push(fetch) });
+            const memory = client.util.bytesToSize(process.memoryUsage().heapUsed);
+            const total = client.util.bytesToSize(require("os").totalmem);
+            const embed = new MessageEmbed()
+            .setAuthor(`${client.user.tag} Statistic`, client.util.getAvatar(client.user))
+            .setColor("#7289DA")
+            .setThumbnail(client.util.getGuildIcon(client.guilds.cache.get("332877090003091456")))
+            .addField("📊 Information", `
+\`\`\`
 Servers            : ${client.guilds.cache.size}
 Users              : ${client.users.cache.size}
 Ping               : ${client.ws.ping} ms
 \`\`\`
-        `)
-        .addField("⚙ System Information", `
-\`\`\`css
+            `)
+            .addField("⚙ System Information", `
+\`\`\`
 CPU           : ${require("os").cpus()[0].model}
 CPU Core      : ${require("os").cpus().length}
 Memory        : ${memory} / ${total}
 Node.js       : ${process.version}
 Discord.js    : v${version}
 Bot Uptime    : ${client.util.parseDur(client.uptime)}
-Server Uptime : ${uptime.slice(3)}
+${uptime !== "" ? `Server Uptime : ${uptime.slice(3)}` : ""}
 \`\`\`
-        `)
-        .addField("👑 Owner Of This Bot", `
+            `)
+            .addField("👑 Owner Of This Bot", `
 \`\`\`css
 ${owner.map(o => `${o.tag} (${o.id})`).join("\n")}
 \`\`\`
-        `)
-        .addField("📌 Additional", `
-Feel free to contribute this bot : https://github.com/zealcordNation/baldguy-bot
-Our Website : https://zealcord.xyz
-        `)
+            `)
+            .addField("📌 Additional", `
+\`\`\`ini
+[ Thanks For Your Contribute To ZealCord ]
+\`\`\`
+[**GitHub Repo**](https://github.com/zealcordNation/baldguy-bot)
+[**Our Website**](https://zealcord.xyz)
+[**KaryaKarsa**](https://karyakarsa.com/zealcord)
+[**Instagram**](https://www.instagram.com/zealcord/)
+[**YouTube**](https://www.youtube.com/channel/UCnlD5sWvlxNqKiyE6RWufVw)
+            `)
         .setFooter(`• Message For : ${message.author.tag}`, client.util.getAvatar(message.author))
         .setTimestamp();
         message.channel.send(embed);
+        });
     }
 }
 
