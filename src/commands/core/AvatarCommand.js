e6c06b3f7231e0db0bddf43abd08ee544740e5d5
HYDROX <40756526+hydraindia@users.noreply.github.com>
2020-03-11 14:33:45 +0530

    Create AvatarCommand.js

---
+++ src/commands/core/AvatarCommand.js
@@ -0,0 +1,35 @@
+const Command = require("../../handler/Command");
+
+/**
+ * @extends {Command}
+ */
+class AvatarCommand extends Command {
+    constructor() {
+        super();
+        this.name = "avatar";
+        this.aliases = ["pfp"];
+        this.info = {
+            desc: "Shows a user profile picture",
+            usage: "avatar [user]"
+        };
+    }
+
+    /**
+     * @param {import("../../handler/BaldClient")} client
+     * @param {import("discord.js").Message} message
+     * @param {Array<String>} args
+     */
+    exec(client, message, args) {
+
+    let user = message.mentions.users.first(); // Mentioned user
+    if (!user) user = message.author; // If there is no user mention it will return message.author as a user
+    
+    let image = user.displayAvatarURL; // Get image URL
+    let embed = new Discord.RichEmbed()
+        .setColor("#0000000") // Set color (If you don't have ideas or preference, use RANDOM for random colors)
+        .setImage(image) // Set image in embed
+        message.channel.send(embed); // Send embed
+}
+}
+
+module.exports = AvatarCommand;
