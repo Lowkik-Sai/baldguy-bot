2d8d43b88ee028306b824cde64476bd78ffceed9
KurokuTetsuya <perumnas.good@gmail.com>
2020-03-15 15:56:15 +0700

    Add reload function for command

--- src/structures/BaseCommand.js
+++ src/structures/BaseCommand.js
@@ -24,6 +24,15 @@ class Command {
      * @param {Array<String>} args
      */
     exec(client, message, args) {}
+
+    reload() {
+        delete require.cache[require.resolve(`${this.path}`)];
+        const newCMD = new (require(`${this.path}`).default)();
+        this.client.commands.get(this.help.name).run = newCMD.run;
+        this.client.commands.get(this.help.name).help = newCMD.help;
+        this.client.commands.get(this.help.name).conf = newCMD.conf;
+        return this.client.commands.get(this.help.name);
+    }
 }
 
 module.exports = Command;
